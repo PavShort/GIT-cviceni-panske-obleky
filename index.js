a7451e26fe6098d56dcc753b5d2b993fa2a28048
Markéta Musílková <mrs.musilkova@gmail.com>
2024-04-05 09:55:08 +0200

    Přidání obarvovacích tlačítek

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('funguju');
+
+
+const changeColor = (color) => {
+  const svgElement = document.getElementById('product-image');
+  svgElement.style.fill = color; 
+};
+
+document.querySelector('.white button').addEventListener('click', () => {
+  changeColor("rgb(245, 236, 236)")
+});
+
+document.querySelector('.red button').addEventListener('click', () => {
+  changeColor("rgb(188, 13, 13)")
+});
+
+document.querySelector('.blue button').addEventListener('click', () => {
+  changeColor("rgb(41, 116, 227)")
+});
